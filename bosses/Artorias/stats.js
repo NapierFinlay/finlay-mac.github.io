e48da8e19f7c30e570474924798e8f098c4d318d
NapierFinlay <158498426+NapierFinlay@users.noreply.github.com>
2024-04-20 13:46:14 +0100

    Add files via upload

---
+++ bosses/Artorias/stats.js
@@ -0,0 +1,21 @@
+var stats = ["HP: 550", "ATK: 300", "DEF: 110", "SPE: 400", "INT: 90"];
+
+let back = document.getElementById("color-back").style.backgroundColor
+
+// https://www.w3schools.com/js/js_functions.asp
+function showStats() {
+	var displayDiv = document.getElementById("variableDisplay");
+	var listHTML = "<ul>";
+
+	// https://www.w3schools.com/jsref/jsref_foreach.asp
+	stats.forEach(function(variable) {
+		listHTML += "<li>" + variable + "</li>";
+	});
+
+  listHTML += "</ul>";
+  displayDiv.innerHTML = listHTML;
+}
+
+// https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+document.getElementById("statsButton").addEventListener("click", showStats);
+
